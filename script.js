5590f5285a212cf9fc9f75768706212b0451d0a5
Cracks07 <gj.goncalvescaldas@gmail.com>
2024-06-10 18:15:56 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const welcomeButton = document.getElementById("welcome-button");
     welcomeButton.addEventListener("click", function() {
-        alert("Welcome to my professional profile!");
+        alert("¡Bienvenidos a mi perfil profesional!");
     });
 });
