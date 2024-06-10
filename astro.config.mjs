a04ac7c21763c8a4f65bc1dbbd9caa0f97018978
Cracks07 <gj.goncalvescaldas@gmail.com>
2024-06-10 17:57:17 +0200

    a

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://gj-goncalvescaldas.github.io/',
-  base: '/',
-  output: 'static',
-});
---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeButton = document.getElementById("welcome-button");
+    welcomeButton.addEventListener("click", function() {
+        alert("Welcome to my professional profile!");
+    });
+});
