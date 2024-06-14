13d3537979b1428d24557e1aad26bea5a090631f
Cracks07 <gj.goncalvescaldas@gmail.com>
2024-06-14 23:10:51 +0200

    test

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const welcomeButton = document.getElementById("welcome-button");
+    const themeToggle = document.getElementById("theme-toggle");
+
     welcomeButton.addEventListener("click", function() {
         alert("¡Bienvenidos a mi perfil profesional!");
+    });
+
+    themeToggle.addEventListener("change", function() {
+        if (themeToggle.checked) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
     });
 });
